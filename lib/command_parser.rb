e485fec747868aa4418e8b54f77c868085cc412d
fap <fap-@users.noreply.github.com>
2016-01-08 16:42:00 +0100

    Adds support for custom quotefiles

--- lib/command_parser.rb
+++ lib/command_parser.rb
@@ -2,13 +2,14 @@ require 'optparse'
 
 # wrapper class for options parser
 class CommandParser
-    # pass the config so we can access
-    def initialize config
-        @config = config
+
+  attr_writer :quotes
+
+    def initialize
         @options = options
     end
 
-    # default options 
+    # default options
     def options
         { method: nil, arguments: nil}
     end
@@ -24,14 +25,18 @@ class CommandParser
             opts.on('-m [name]', '--motivate [name]', String, "# Output a motivational quote") do |m|
                 @options[:method] = 'motivate'
                 @options[:arguments] = { author: m }
-            end            
+            end
 
             opts.on('-w [glob1,glob2] ', '--watch [glob1,glob2]', Array, '# Watch files & output a message on file/folder change event') do |w|
                 @options[:method] = 'watch'
                 @options[:arguments] = { files: w }
             end
 
-            opts.on_tail('-h', '--help', '# Output command line help options') do 
+            opts.on('-q [quotefile]', '--quotes [quotefile]', String, '# Use a specific quotefile') do |q|
+              @options[:quotes] = q
+            end
+
+            opts.on_tail('-h', '--help', '# Output command line help options') do
                 puts opts
                 exit
             end
@@ -40,7 +45,7 @@ class CommandParser
         # set the options from calling the parser if there are any exceptions
         # we need to catch them and output the help message
         begin
-            parser.parse!(arguments)    
+            parser.parse!(arguments)
         rescue Exception => e
             puts parser
             exit(1)
@@ -48,7 +53,7 @@ class CommandParser
 
         # if no valid methods were set we can't execute and therefore we exit and
         # put the parser message to the console
-        if @options[:method].nil?            
+        if @options[:method].nil?
             puts parser
             exit(1)
         end
@@ -57,11 +62,11 @@ class CommandParser
         @options
     end
 
-    #output a prett list of authors
+    #output a pretty list of authors
     def output_authors
         authors = []
 
-        @config['quotes'].each do |author|
+        quotes.each do |author|
             authors << author[0]
         end
 
